5403182f4b257aecfe0cbec28a0896cd85ad1a19
ssmousav1 <sobhanmousavi121122@gmail.com>
2025-07-20 12:21:40 +0330

    add prooduct context

---
+++ frontend/src/contexts/ProductContext.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Product, ProductContextType, Review } from '@/types';
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+
+
+const ProductContext = createContext<ProductContextType | undefined>(undefined);
+
+export const ProductProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+  const addProduct = (product: Product) => {
+    setProducts([...products, product]);
+  };
+
+  const addReview = (productId: string, review: Review) => {
+    setProducts(products.map(product => 
+      product.id === productId 
+        ? { ...product, reviews: [...product.reviews, review] }
+        : product
+    ));
+    
+    if (selectedProduct?.id === productId) {
+      setSelectedProduct({
+        ...selectedProduct,
+        reviews: [...selectedProduct.reviews, review]
+      });
+    }
+  };
+
+  return (
+    <ProductContext.Provider value={{
+      products,
+      selectedProduct,
+      setProducts,
+      setSelectedProduct,
+      addProduct,
+      addReview
+    }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export const useProduct = () => {
+  const context = useContext(ProductContext);
+  return context;
+};
